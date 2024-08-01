a33ad3475a426ce8f0f20b772eaf6846408f2476
FleexVenegas <diego.ven80@gmail.com>
2024-08-01 17:51:58 -0600

    card

--- src/renderer/src/components/molecules/TaskView/TaskView.tsx
+++ src/renderer/src/components/molecules/TaskView/TaskView.tsx
@@ -5,8 +5,9 @@ const TaskView = () => {
     const info = [
         {
             id: '1',
-            title: 'Esta es una tarea',
+            title: 'Bugs',
             date: '01/07/2024',
+            date_: '01/07/2024',
             description:
                 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto non eaque magnam fugit voluptates molestiae.',
             is_active: true
@@ -17,24 +18,29 @@ const TaskView = () => {
         <div className="TaskView">
             {info.map((_, idx) => (
                 <div className="t-cnt-task" key={idx}>
-                    <div className='t-cnt-up'>
-                        <div className='t-header'>
-                            <h3 className='t-tile'>{_.title}</h3>
-                            <span className='t-date'>{_.date}</span>
+                    <div className="t-cnt-up">
+                        <div className="t-header">
+                            <h3 className="t-tile">{_.title}</h3>
+                            <div className="t-cnt-dates">
+                                <span className="t-date">{_.date}</span>
+                                -
+                                <span className="t-date">{_.date_}</span>
+                            </div>
                         </div>
-                        <div className='t-cnt-body'>
-                            <p className='t-description'>{_.description}</p>
+                        <div className="t-cnt-body">
+                            <span className='s-title-spa'>Descripción</span>
+                            <p className="t-description">{_.description}</p>
                         </div>
                     </div>
 
-                    <div>
-                        <Toggle />
+                    <div className="t-cnt-sta">
+                        <span className='s-title-spa'>Status</span>
+                        <Toggle initialValue={_.is_active} />
                     </div>
 
-                    <button className='t-btn-close'>✖️</button>
+                    <button className="t-btn-close">✖️</button>
                 </div>
             ))}
-
         </div>
     )
 }
